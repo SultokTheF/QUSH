27f53779a4ac49e6305d5a53083061dfd215bbd3
diasKarataev <123745326+diasKarataev@users.noreply.github.com>
2023-07-08 21:41:48 +0600

    chore: Delete api directory

--- api/FIELD/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FieldViews, CreateTicketView
-urlpatterns = [
-    path('field/fields/', FieldViews.as_view(), name='FIELD-list'),
-    path('field/fields/<str:pk>/', FieldViews.as_view(), name='FIELD-item'),
-    path('field/create-tickets/<int:time_from>/<int:time_to>/<int:field_id>', CreateTicketView.as_view())
-]
